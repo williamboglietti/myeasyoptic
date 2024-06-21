ec542f10fd65ae3d35c49e5dd93c34a02118593c
williamboglietti <williamboglietti@users.noreply.github.com>
2024-06-21 03:24:54 +0200

    Update

--- src/install.cmd
+++ src/install.cmd
@@ -4,8 +4,12 @@ REM Vérifier les droits administrateur en vérifiant l'accès au dossier systè
 
 mkdir "%systemdrive%\Temp\AdminCheck" >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Vous n'avez pas les droits administrateur.
+    echo Set objShell = CreateObject("WScript.Shell") > "%temp%\admincheck.vbs"
+    echo objShell.Popup "Vous n'avez pas les droits administrateur.", 0, "Droits administrateur", 0 + 48 > "%temp%\admincheck.vbs"
 ) else (
-    echo Vous avez les droits administrateur.
+    echo Set objShell = CreateObject("WScript.Shell") > "%temp%\admincheck.vbs"
+    echo objShell.Popup "Vous avez les droits administrateur.", 0, "Droits administrateur", 0 + 64 > "%temp%\admincheck.vbs"
 )
 rmdir "%systemdrive%\Temp\AdminCheck" >nul 2>&1
+wscript.exe "%temp%\admincheck.vbs"
+del "%temp%\admincheck.vbs"
